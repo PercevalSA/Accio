25fae02eab2297120f04f2cdf76c85281fe39dfd
delcourt <samuel.delcourt@telecom-paristech.fr>
2015-02-03 22:12:21 +0100

    Création d'une classe pour les boites et une pour les frigos

---
+++ app/src/main/java/com/delcourt/samuel/accio/structures/Boite.java
@@ -0,0 +1,11 @@
+package com.delcourt.samuel.accio.structures;
+
+/* Définit la structure des boîtes.
+Les attributs de la boîte seront données par la base de bonnées
+ */
+
+public class Boite {
+
+    public String name;
+    public String référenceBdd; //référence vers la base de données : permet d'avoir accès aux informations sur la boîte
+}
---
+++ app/src/main/java/com/delcourt/samuel/accio/structures/Refrigerateur.java
@@ -0,0 +1,16 @@
+package com.delcourt.samuel.accio.structures;
+
+import java.util.ArrayList;
+
+/* Définit la structure des réfrigérateurs.
+Le nom des frigo et les références sur la base de données des boites qu'ils contiennent
+sont ainsi stockées par l'application elle même.
+ */
+
+public class Refrigerateur {
+
+    public String name;
+    public ArrayList boites;
+    
+
+}
