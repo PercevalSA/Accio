19e64153a7fb6b21bfdf7f7f9e95bce6b7e01103
delcourt <samuel.delcourt@telecom-paristech.fr>
2015-02-03 16:57:29 +0100

    Modification de noms (cause d'erreurs)

--- app/src/main/java/com/delcourt/samuel/accio/ContentSousActivites/ContentLegumesActivity.java
+++ app/src/main/java/com/delcourt/samuel/accio/ContentSousActivites/ContentLegumesActivity.java
@@ -12,14 +12,14 @@ public class ContentLegumesActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contentLegumes);
+        setContentView(R.layout.activity_contentlegumes);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_contentLegumes, menu);
+        getMenuInflater().inflate(R.menu.menu_contentlegumes, menu);
         return true;
     }
 
