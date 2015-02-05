05c019024186c1c5b8b25860af89b438bac78887
delcourt <samuel.delcourt@telecom-paristech.fr>
2015-02-05 20:37:54 +0100

    Création d'un frigo par défaut à l'ouverture de l'appli (reverted from commit dc4ea9206dd7bd2f60112521c4e44264182eebcf)

--- app/src/main/java/com/delcourt/samuel/accio/MainActivity.java
+++ app/src/main/java/com/delcourt/samuel/accio/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createFrigo("frigoParDefaut");
     }
 
 
