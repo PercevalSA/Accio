37550a8ba86234834351790b2f9741420cec8607
delcourt <samuel.delcourt@telecom-paristech.fr>
2015-02-16 18:46:55 +0100

    Mise en place de NewBoxActivity

--- app/src/main/java/com/delcourt/samuel/accio/create_new_object_activities/NewBoxActivity.java
+++ app/src/main/java/com/delcourt/samuel/accio/create_new_object_activities/NewBoxActivity.java
@@ -5,15 +5,45 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.delcourt.samuel.accio.R;
 
+import java.util.ArrayList;
+
 public class NewBoxActivity extends ActionBarActivity {
+
+    public ArrayList<String> listTypesBoxes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_box);
+
+        // Get the reference of List
+        ListView animalList=(ListView)findViewById(R.id.listViewTypeBox);
+
+        getTypesNames();
+        // Create The Adapter with passing ArrayList as 3rd parameter
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listTypesBoxes);
+        // Set The Adapter
+        animalList.setAdapter(arrayAdapter);
+
+        //register onClickListener to handle click events on each item
+        animalList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            // argument position gives the index of item which is clicked
+            public void onItemClick(AdapterView<?> arg0, View v,int position, long arg3)
+            {
+
+                String selected=listTypesBoxes.get(position);
+
+            }
+        });
     }
 
 
@@ -45,5 +75,14 @@ public class NewBoxActivity extends ActionBarActivity {
 
     public void sendMessageHelp(View view){
 
+    }
+
+    void getTypesNames() {
+        listTypesBoxes.add("Fruits");
+        listTypesBoxes.add("Légumes");
+        listTypesBoxes.add("Produits laitiers");
+        listTypesBoxes.add("Poisson");
+        listTypesBoxes.add("Viande");
+        listTypesBoxes.add("Sauces et condiments");
     }
 }
