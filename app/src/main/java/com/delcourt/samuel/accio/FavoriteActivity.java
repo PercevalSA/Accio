5ba1348b7a243c5da181948f721126dbedbc39c3
delcourt <samuel.delcourt@telecom-paristech.fr>
2015-03-08 22:16:30 +0100

    Passage par un fichier texte

--- app/src/main/java/com/delcourt/samuel/accio/FavoriteActivity.java
+++ app/src/main/java/com/delcourt/samuel/accio/FavoriteActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.delcourt.samuel.accio.interaction.BDD;
 
@@ -27,9 +28,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FavoriteActivity extends ActionBarActivity {
 
@@ -47,6 +53,24 @@ public class FavoriteActivity extends ActionBarActivity {
 
         new BDD2().execute();//On se connecte à la bdd. Normalement, listeAlimentsAffichage doit se remplir
 
+        //Lecture du fichier texte possédant le nom des aliments
+
+        InputStream instream;
+        try {
+            instream = openFileInput("aliments.txt");
+            InputStreamReader inputreader = new InputStreamReader(instream);
+            BufferedReader buffreader = new BufferedReader(inputreader);
+            Scanner sc = new Scanner(buffreader);
+            while(sc.hasNextLine() == true){//On recrée la liste des aliments à afficher
+                String name = sc.nextLine();
+                listeAlimentsAffichage.add(name);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {//A lieu à la première utilisation d'accio. On crée alors le frigo de référence (suite) (utile pour nous)
+            Toast.makeText(getApplicationContext(), "Problème lecture fichier texte",
+                    Toast.LENGTH_SHORT).show();
+        }
+
         //On prépare la ListView
         ListView listAliments=(ListView)findViewById(R.id.listeViewListeAliments1);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, listeAlimentsAffichage);
@@ -59,41 +83,41 @@ public class FavoriteActivity extends ActionBarActivity {
         String result = null;
 
         protected String doInBackground(String... urls) {
-                String result = "";
+            String result = "";
 
-                InputStream is = null;
+            InputStream is = null;
 
-                // aliment recherché
-                ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-                nameValuePairs.add(new BasicNameValuePair("nomCategorie", "Legume"));
-                ArrayList<String> donnees = new ArrayList<String>();
+            // aliment recherché
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("nomCategorie", "Legume"));
+            ArrayList<String> donnees = new ArrayList<String>();
 
-                // Envoi de la requête avec HTTPPost
-                try {
-                    HttpClient httpclient = new DefaultHttpClient();
-                    HttpPost httppost = new HttpPost("http://192.168.0.12/pact/connection2.php");
-                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                    HttpResponse response = httpclient.execute(httppost);
-                    HttpEntity entity = response.getEntity();
-                    is = entity.getContent();
-                } catch (Exception e) {
-                    Log.e("log_tag", "Error in http connection " + e.toString());
+            // Envoi de la requête avec HTTPPost
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost("http://192.168.0.12/pact/connection2.php");
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                is = entity.getContent();
+            } catch (Exception e) {
+                Log.e("log_tag", "Error in http connection " + e.toString());
+            }
+
+            //Conversion de la réponse en chaine
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
                 }
+                is.close();
 
-                //Conversion de la réponse en chaine
-                try {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
-                    StringBuilder sb = new StringBuilder();
-                    String line = null;
-                    while ((line = reader.readLine()) != null) {
-                        sb.append(line + "\n");
-                    }
-                    is.close();
-
-                    result = sb.toString();
-                } catch (Exception e) {
-                    Log.e("log_tag", "Error converting result " + e.toString());
-                }
+                result = sb.toString();
+            } catch (Exception e) {
+                Log.e("log_tag", "Error converting result " + e.toString());
+            }
 
             //Parsing des données JSON
             try {
@@ -107,10 +131,24 @@ public class FavoriteActivity extends ActionBarActivity {
                                     ", Nom: " + json_data.getString("Nom") +
                                     ", Categorie: " + json_data.getString("Categorie")
                     );
+
+                    //On écrit le nom des aliments dans un fichier texte
+                    try {//Crée le fichier contenant la liste des boîtes de ce frigo
+                        OutputStreamWriter outStream = new OutputStreamWriter(openFileOutput("aliments.txt",MODE_PRIVATE));
+                        BufferedWriter bw = new BufferedWriter(outStream);
+                        PrintWriter out2 = new PrintWriter(bw);
+                        out2.println(json_data.getString("Nom"));
+                        out2.close();
+                        Log.i("log_tag", "Ecriture fichiers ok");
+
+                    } catch (java.io.IOException e) {
+                        Log.e("log_tag", "Erreur écriture fichier");
+                    }
+
                     result += "\n\t" + jArray.getJSONObject(i);
 
 
-                    
+
                 }
             } catch (JSONException e) {
                 Log.e("log_tag", "Error parsing data " + e.toString());
@@ -118,7 +156,7 @@ public class FavoriteActivity extends ActionBarActivity {
 
 
 
-                return result;
+            return result;
         }
 
 
@@ -127,23 +165,23 @@ public class FavoriteActivity extends ActionBarActivity {
         protected void onPostExecute(String result) {
 
             //Parsing des données JSON
-           /** try {
-                Log.i("tagconvertstr", "[" + result + "]"); // permet de voir ce que retoune le script. un code html pouquoi ?
-                JSONArray jArray = new JSONArray(result);
-                for (int i = 0; i < jArray.length(); i++) {
-                    JSONObject json_data = jArray.getJSONObject(i);
-                    Log.i("log_tag", "BoiteID: " + json_data.getInt("BoiteID") +
-                                    ", Nom: " + json_data.getString("Nom") +
-                                    ", Categorie: " + json_data.getString("Categorie")
-                    );
-                    result += "\n\t" + jArray.getJSONObject(i);
+            /** try {
+             Log.i("tagconvertstr", "[" + result + "]"); // permet de voir ce que retoune le script. un code html pouquoi ?
+             JSONArray jArray = new JSONArray(result);
+             for (int i = 0; i < jArray.length(); i++) {
+             JSONObject json_data = jArray.getJSONObject(i);
+             Log.i("log_tag", "BoiteID: " + json_data.getInt("BoiteID") +
+             ", Nom: " + json_data.getString("Nom") +
+             ", Categorie: " + json_data.getString("Categorie")
+             );
+             result += "\n\t" + jArray.getJSONObject(i);
 
-                }
-            } catch (JSONException e) {
-                Log.e("log_tag", "Error parsing data " + e.toString());
-            }
+             }
+             } catch (JSONException e) {
+             Log.e("log_tag", "Error parsing data " + e.toString());
+             }
 
-            **/
+             **/
         }
 
     }
