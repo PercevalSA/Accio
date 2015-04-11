7c5f30dab8d79af6f544398b0e518c82db2e66a6
delcourt <samuel.delcourt@telecom-paristech.fr>
2015-04-11 11:21:38 +0200

    Idem renommage boîtes

--- app/src/main/java/com/delcourt/samuel/accio/options_activities/BoxOptionsActivity.java
+++ app/src/main/java/com/delcourt/samuel/accio/options_activities/BoxOptionsActivity.java
@@ -11,6 +11,7 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -49,6 +50,17 @@ public class BoxOptionsActivity extends ActionBarActivity {
         try{
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_box_options);
+
+            //Force l'affichage du clavier
+            EditText editText = (EditText) findViewById(R.id.edit_text_renommer_frigo);
+            editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (hasFocus) {
+                        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                    }
+                }
+            });
 
             //Récupère les informations de la boîte pour les afficher :
             TextView textElement = (TextView) findViewById(R.id.boxName_BoxOptionActivity);
